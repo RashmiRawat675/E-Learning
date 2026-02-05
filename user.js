e9605d87d3500c381850e029795766c27468d1e5
RashmiRawat675 <rashmirawat71871@gmail.com>
2026-02-05 23:50:59 +0530

    Add files via upload

---
+++ user.js
@@ -0,0 +1,116 @@
+import {User} from "../models/user.js";
+import bcrypt from 'bcrypt';
+ import jwt from "jsonwebtoken";
+import sendMail from "../middlewares/sendMail.js";
+import TryCatch from "../middlewares/TryCatch.js";
+
+ export const register = TryCatch(async(req,res) =>{
+     //    res.send("Register Api");
+           const { name,email,password} = req.body
+
+           if ( !name || !email ||  !password) {
+              return res.status(400).json({
+                 message: "All fields are required",
+           });
+          }
+
+           let user = await User.findOne({email})
+
+           if (user) return res.status(400).json({
+            message: "User Alredy exists",
+           });
+
+           const hashpassword = await bcrypt.hash(password, 10)
+
+           user = {
+            name,
+            email,
+            password : hashpassword
+           }
+
+           const  otp = Math.floor(Math.random()*1000000);
+
+           const activationToken = jwt.sign({
+            user,
+            otp,
+           }, process.env.Activation_Secret,
+            {
+                expiresIn:"5m",
+            }
+        );
+
+        const data = {
+           name,
+           otp,
+        };
+
+        await sendMail( email,"E learning",data)
+
+           res.status(200).json({
+            message: "Otp send to Your mail",
+            activationToken,
+           });
+ });
+
+export const verifyUser = TryCatch(async(req,res) => {
+  const {otp, activationToken} = req.body;
+
+  const verify = jwt.verify(activationToken, process.env.Activation_Secret);
+
+  if(!verify)
+    return res.status(400).json({
+        message: "otp Expires",
+    });
+        
+    if (verify.otp !== otp)
+     return res.status(400).json({
+        message: " Wrong otp ",
+    });
+
+    await User.create({
+        name:verify.user.name,
+        email: verify.user.email,
+        password: verify.user.password,
+    })
+
+    res.json({
+        message: "User Registered"
+    })
+  
+});
+
+export const loginUser = TryCatch(async(req,res) => {
+    const {email,password} = req.body;
+
+    const user = await User.findOne({email});
+
+    if(!user) 
+         
+        return res.status(400).json({
+            message: "No User with this email",
+        });
+
+      const matchPassword =   await bcrypt.compare(password, user.password);
+
+      if (!matchPassword) 
+        return res.status(400).json({
+          message: "Wrong Password",
+    });
+
+    const token = jwt.sign({_id: user._id}, process.env.jwt_sec,
+        {
+            expiresIn : "15d",
+        });
+
+        res.json({
+            message: `Welcome back ${user.name}`,
+            token,
+            user,
+        });
+});
+ 
+export const myProfile = TryCatch(async(req,res) =>{
+    const user = await User.findById(req.user._id);
+
+    res.json({user});
+});
